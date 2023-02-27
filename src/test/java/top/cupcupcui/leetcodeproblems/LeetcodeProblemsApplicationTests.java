c032f49a2fc544c12de97458349aa399c4894015
cuiguanghao <cuiguanghao@ultrapower.com.cn>
2023-02-27 22:11:59 +0800

    init project

---
+++ src/test/java/top/cupcupcui/leetcodeproblems/LeetcodeProblemsApplicationTests.java
@@ -0,0 +1,13 @@
+package top.cupcupcui.leetcodeproblems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeProblemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
