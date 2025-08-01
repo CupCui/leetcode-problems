98f64a7fb4ec86e6f1db14dcb520ff70272202b9
cuiguanghao <cuiguanghao@nantian.com.cn>
2025-08-01 10:13:23 +0800

    380. O(1) 时间插入、删除和获取随机元素，第一版本提交，未自测

--- src/main/java/top/cupcupcui/leetcodeproblems/topinterview150/InsertDeleteGetRandomO1.java
+++ src/main/java/top/cupcupcui/leetcodeproblems/topinterview150/InsertDeleteGetRandomO1.java
@@ -1,6 +1,7 @@
 package top.cupcupcui.leetcodeproblems.topinterview150;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @author cuiguanghao
@@ -88,26 +89,38 @@ public class InsertDeleteGetRandomO1 {
         }
 
         public boolean insert(int val) {
-            nums.add(val);
-            return true;
+            // bool insert(int val) 当元素 val 不存在时，向集合中插入该项，并返回 true ；否则，返回 false 。
+            // randomizedSet.insert(1); // 向集合中插入 1 。返回 true 表示 1 被成功地插入。
+            boolean result = false;
+            if (nums.contains(val)) {
+                result = false;
+            } else {
+                nums.add(val);
+                result = true;
+            }
+            String numsString = Arrays.toString(nums.toArray());
+            System.out.println(numsString);
+            return result;
         }
 
         public boolean remove(int val) {
-            Integer neededDeleteNum = null;
-            for (Integer num : nums) {
-                if (num == val) {
-                    neededDeleteNum = val;
-                }
-            }
-            if (neededDeleteNum != null) {
-                nums.remove(neededDeleteNum);
-                return true;
+            // bool remove(int val) 当元素 val 存在时，从集合中移除该项，并返回 true ；否则，返回 false 。
+            // randomizedSet.remove(2); // 返回 false ，表示集合中不存在 2 。
+            boolean result = false;
+            if (nums.contains(val)) {
+                nums.remove(Integer.valueOf(val));
+                result = true;
             } else {
-                return false;
+                result = false;
             }
+            String numsString = Arrays.toString(nums.toArray());
+            System.out.println(numsString);
+            return result;
         }
 
         public int getRandom() {
+            // int getRandom() 随机返回现有集合中的一项（测试用例保证调用此方法时集合中至少存在一个元素）。每个元素应该有 相同的概率 被返回。
+            // randomizedSet.getRandom(); // getRandom 应随机返回 1 或 2 。
             int i = new Random().nextInt(nums.size());
             return nums.get(i);
         }
--- src/test/java/top/cupcupcui/leetcodeproblems/topinterview150/InsertDeleteGetRandomO1Test.java
+++ src/test/java/top/cupcupcui/leetcodeproblems/topinterview150/InsertDeleteGetRandomO1Test.java
@@ -43,6 +43,55 @@ public class InsertDeleteGetRandomO1Test {
 
     }
 
+    /**
+     * 输入
+     * ["RandomizedSet", "insert", "remove", "insert", "getRandom", "remove", "insert", "getRandom"]
+     * [[], [1], [2], [2], [], [1], [2], []]
+     * 输出
+     * [null, true, false, true, 2, true, false, 2]
+     * <p>
+     * 解释
+     * RandomizedSet randomizedSet = new RandomizedSet();
+     * randomizedSet.insert(1); // 向集合中插入 1 。返回 true 表示 1 被成功地插入。
+     * randomizedSet.remove(2); // 返回 false ，表示集合中不存在 2 。
+     * randomizedSet.insert(2); // 向集合中插入 2 。返回 true 。集合现在包含 [1,2] 。
+     * randomizedSet.getRandom(); // getRandom 应随机返回 1 或 2 。
+     */
+    @Test
+    public void jumpGame_PositiveCase1() {
+        InsertDeleteGetRandomO1.RandomizedSet obj = new InsertDeleteGetRandomO1.RandomizedSet();
+        /**
+         * 输入
+         * ["RandomizedSet", "insert", "remove", "insert", "getRandom", "remove", "insert", "getRandom"]
+         * [[], [1], [2], [2], [], [1], [2], []]
+         * 输出
+         * [null, true, false, true, 2, true, false, 2]
+         */
+        boolean param_1 = obj.insert(1);
+        boolean param_2 = obj.remove(2);
+        boolean param_3 = obj.insert(2);
+        int param_4 = obj.getRandom();
+        boolean param_5 = obj.remove(1);
+        boolean param_6 = obj.insert(2);
+        int param_7 = obj.getRandom();
+
+        boolean expected1 = true;
+        boolean expected2 = false;
+        boolean expected3 = true;
+        int expected4_1 = 1;
+        int expected4_2 = 2;
+        boolean expected5 = true;
+        boolean expected6 = false;
+        int expected7_2 = 2;
+        Assert.assertEquals(expected1, param_1);
+        Assert.assertEquals(expected2, param_2);
+        Assert.assertEquals(expected3, param_3);
+        Assert.assertTrue(Objects.equals(expected4_1, param_4) || Objects.equals(expected4_2, param_4));
+        Assert.assertEquals(expected5, param_5);
+        Assert.assertEquals(expected6, param_6);
+        Assert.assertTrue(Objects.equals(expected7_2, param_7));
+    }
+
 
     /**
      * leet 官方题解
