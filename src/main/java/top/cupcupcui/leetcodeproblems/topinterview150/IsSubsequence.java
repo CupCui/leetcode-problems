f0624b6f378f22914ae4cd34e35aaba21b52a600
cuiguanghao <cuiguanghao@nantian.com.cn>
2025-11-06 13:48:58 +0800

    392. 判断子序列，初始提交

---
+++ src/main/java/top/cupcupcui/leetcodeproblems/topinterview150/IsSubsequence.java
@@ -0,0 +1,57 @@
+package top.cupcupcui.leetcodeproblems.topinterview150;
+
+/**
+ * @author cuiguanghao
+ * @date 2025/11/6 13:39
+ * @description 392. 判断子序列
+ */
+public class IsSubsequence {
+
+    /**
+     * https://leetcode.cn/problems/is-subsequence/description/?envType=study-plan-v2&envId=top-interview-150
+     * 392. 判断子序列
+     * 给定字符串 s 和 t ，判断 s 是否为 t 的子序列。
+     * 字符串的一个子序列是原始字符串删除一些（也可以不删除）字符而不改变剩余字符相对位置形成的新字符串。（例如，"ace"是"abcde"的一个子序列，而"aec"不是）。
+     * 进阶：
+     * 如果有大量输入的 S，称作 S1, S2, ... , Sk 其中 k >= 10亿，你需要依次检查它们是否为 T 的子序列。在这种情况下，你会怎样改变代码？
+     *
+     * 示例 1：
+     * 输入：s = "abc", t = "ahbgdc"
+     * 输出：true
+     *
+     * 示例 2：
+     * 输入：s = "axc", t = "ahbgdc"
+     * 输出：false
+     *
+     * 提示：
+     * 0 <= s.length <= 100
+     * 0 <= t.length <= 10^4
+     * 两个字符串都只由小写字符组成。
+     *
+     * 相关标签
+     * 双指针 字符串 动态规划
+     */
+
+    /**
+     * 思路：双指针；在原字符串上直接判断
+     * -[]
+     * 复杂度分析
+     * 时间复杂度：O()
+     * 空间复杂度：O()
+     * 相关标签:
+     * 测试:
+     * 结果:
+     * 优化建议：
+     * 核心思路是：
+     * 空间优化：
+     *
+     * @param s
+     * @param t
+     * @return
+     */
+    public boolean isSubsequence(String s, String t) {
+
+        return false;
+    }
+
+}
---
+++ src/test/java/top/cupcupcui/leetcodeproblems/topinterview150/IsSubsequenceTest.java
@@ -0,0 +1,115 @@
+package top.cupcupcui.leetcodeproblems.topinterview150;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IsSubsequenceTest {
+
+    /**
+     * 输入：s = "abc", t = "ahbgdc"
+     * 输出：true
+     */
+    @Test
+    public void test_PositiveCase0() {
+        IsSubsequence service = new IsSubsequence();
+        String arg1 = "abc";
+        String arg2 = "ahbgdc";
+        boolean expected = true;
+        boolean response = service.isSubsequence(arg1, arg2);
+
+        Assert.assertEquals(expected, response);
+    }
+
+    /**
+     * 输入：s = "axc", t = "ahbgdc"
+     * 输出：false
+     */
+    @Test
+    public void test_PositiveCase1() {
+        IsSubsequence service = new IsSubsequence();
+        String arg1 = "axc";
+        String arg2 = "ahbgdc";
+        boolean expected = false;
+        boolean response = service.isSubsequence(arg1, arg2);
+
+        Assert.assertEquals(expected, response);
+    }
+
+    /**
+     * 输入：s = "", t = ""
+     * 输出：true
+     */
+    @Test
+    public void test_PositiveCase2() {
+        IsSubsequence service = new IsSubsequence();
+        String arg1 = "";
+        String arg2 = "";
+        boolean expected = true;
+        boolean response = service.isSubsequence(arg1, arg2);
+
+        Assert.assertEquals(expected, response);
+    }
+
+    /**
+     * 输入：s = "a", t = "a"
+     * 输出：true
+     */
+    @Test
+    public void test_PositiveCase3() {
+        IsSubsequence service = new IsSubsequence();
+        String arg1 = "a";
+        String arg2 = "a";
+        boolean expected = true;
+        boolean response = service.isSubsequence(arg1, arg2);
+
+        Assert.assertEquals(expected, response);
+    }
+
+    /**
+     * 输入：s = "ab", t = "abc"
+     * 输出：true
+     */
+    @Test
+    public void test_PositiveCase4() {
+        IsSubsequence service = new IsSubsequence();
+        String arg1 = "ab";
+        String arg2 = "abc";
+        boolean expected = true;
+        boolean response = service.isSubsequence(arg1, arg2);
+
+        Assert.assertEquals(expected, response);
+    }
+
+    /**
+     * 输入：s = "ac", t = "abc"
+     * 输出：true
+     */
+    @Test
+    public void test_PositiveCase5() {
+        IsSubsequence service = new IsSubsequence();
+        String arg1 = "ac";
+        String arg2 = "abc";
+        boolean expected = true;
+        boolean response = service.isSubsequence(arg1, arg2);
+
+        Assert.assertEquals(expected, response);
+    }
+
+    /**
+     * 输入：s = "ab", t = "ac"
+     * 输出：false
+     */
+    @Test
+    public void test_PositiveCase6() {
+        IsSubsequence service = new IsSubsequence();
+        String arg1 = "ab";
+        String arg2 = "ac";
+        boolean expected = false;
+        boolean response = service.isSubsequence(arg1, arg2);
+
+        Assert.assertEquals(expected, response);
+    }
+
+}
