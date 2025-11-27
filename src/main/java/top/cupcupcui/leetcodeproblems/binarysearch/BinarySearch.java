4a856bc9d3898f8effa1fb3d464acafe03576a2e
cuiguanghao <cuiguanghao@nantian.com.cn>
2025-11-27 17:11:20 +0800

    704. 二分查找，初始提交

---
+++ src/main/java/top/cupcupcui/leetcodeproblems/binarysearch/BinarySearch.java
@@ -0,0 +1,53 @@
+package top.cupcupcui.leetcodeproblems.binarysearch;
+
+/**
+ * @author cuiguanghao
+ * @date 2025/11/27 16:57
+ * @description 704. 二分查找
+ */
+public class BinarySearch {
+    /**
+     * https://leetcode.cn/problems/binary-search/?envType=study-plan-v2&envId=binary-search
+     * 704. 二分查找
+     * 简单
+     *
+     * 给定一个 n 个元素<b>有序</b>的（<b>升序</b>）<b>整型</b>数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的 target，如果 target 存在返回下标，否则返回 -1。
+     * 你必须编写一个具有 O(log n) 时间复杂度的算法。
+     *
+     * 示例 1:
+     * 输入: nums = [-1,0,3,5,9,12], target = 9
+     * 输出: 4
+     * 解释: 9 出现在 nums 中并且下标为 4
+     *
+     * 示例 2:
+     * 输入: nums = [-1,0,3,5,9,12], target = 2
+     * 输出: -1
+     * 解释: 2 不存在 nums 中因此返回 -1
+     *
+     * 提示：
+     * 你可以假设 nums 中的所有元素是<b>不重复</b>的。
+     * n 将在 [1, 10000]之间。
+     * nums 的每个元素都将在 [-9999, 9999]之间。
+     *
+     * 相关标签
+     * 数组 二分查找
+     *
+     */
+
+    /**
+     * 思路：二分法
+     * 时间复杂度：O()
+     * 空间复杂度：O()
+     * 结果:
+     * 优化建议：
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int search(int[] nums, int target) {
+
+        return -1;
+    }
+
+}
---
+++ src/test/java/top/cupcupcui/leetcodeproblems/binarysearch/BinarySearchTest.java
@@ -0,0 +1,100 @@
+package top.cupcupcui.leetcodeproblems.binarysearch;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BinarySearchTest {
+
+    /**
+     * 输入: nums = [-1,0,3,5,9,12], target = 9
+     * 输出: 4
+     */
+    @Test
+    public void test_PositiveCase0() {
+        BinarySearch service = new BinarySearch();
+        int[] arg1 = {-1, 0, 3, 5, 9, 12};
+        int arg2 = 9;
+        int expected = 4;
+        int response = service.search(arg1, arg2);
+
+        Assert.assertEquals(expected, response);
+    }
+
+    /**
+     * 输入: nums = [-1,0,3,5,9,12], target = 2
+     * 输出: -1
+     */
+    @Test
+    public void test_PositiveCase1() {
+        BinarySearch service = new BinarySearch();
+        int[] arg1 = {-1, 0, 3, 5, 9, 12};
+        int arg2 = 2;
+        int expected = -1;
+        int response = service.search(arg1, arg2);
+
+        Assert.assertEquals(expected, response);
+    }
+
+    /**
+     * 输入: nums = [0], target = 0
+     * 输出: 0
+     */
+    @Test
+    public void test_PositiveCase2() {
+        BinarySearch service = new BinarySearch();
+        int[] arg1 = {0};
+        int arg2 = 0;
+        int expected = 0;
+        int response = service.search(arg1, arg2);
+
+        Assert.assertEquals(expected, response);
+    }
+
+    /**
+     * 输入: nums = [0], target = 1
+     * 输出: -1
+     */
+    @Test
+    public void test_PositiveCase3() {
+        BinarySearch service = new BinarySearch();
+        int[] arg1 = {0};
+        int arg2 = 1;
+        int expected = -1;
+        int response = service.search(arg1, arg2);
+
+        Assert.assertEquals(expected, response);
+    }
+
+    /**
+     * 输入: nums = [0, 1], target = -1
+     * 输出: -1
+     */
+    @Test
+    public void test_PositiveCase4() {
+        BinarySearch service = new BinarySearch();
+        int[] arg1 = {0, 1};
+        int arg2 = -1;
+        int expected = -1;
+        int response = service.search(arg1, arg2);
+
+        Assert.assertEquals(expected, response);
+    }
+
+    /**
+     * 输入: nums = [0, 1], target = 1
+     * 输出: 1
+     */
+    @Test
+    public void test_PositiveCase5() {
+        BinarySearch service = new BinarySearch();
+        int[] arg1 = {0, 1};
+        int arg2 = 1;
+        int expected = 1;
+        int response = service.search(arg1, arg2);
+
+        Assert.assertEquals(expected, response);
+    }
+
+}
