aa845249863b83c943ce56222e19e65ce93dfddb
cuiguanghao <cuiguanghao@nantian.com.cn>
2025-12-19 18:28:15 +0800

    1351. 统计有序矩阵中的负数，二分法初步提交，未测试通过

--- src/main/java/top/cupcupcui/leetcodeproblems/binarysearch/CountNegativeNumbersInASortedMatrix.java
+++ src/main/java/top/cupcupcui/leetcodeproblems/binarysearch/CountNegativeNumbersInASortedMatrix.java
@@ -11,10 +11,13 @@ public class CountNegativeNumbersInASortedMatrix {
      * 1351. 统计有序矩阵中的负数
      * 简单
      * 提示
-     * 给你一个 m * n 的矩阵 grid，矩阵中的元素无论是按行还是按列，都以非严格递减顺序排列。 请你统计并返回 grid 中 负数 的数目。
+     * 给你一个 m * n 的矩阵 grid，矩阵中的元素无论是按行还是按列，都以<b>非严格递减顺序</b>排列。 请你统计并返回 grid 中 <b>负数</b> 的数目。
      *
      * 示例 1：
-     * 输入：grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
+     * 输入：grid = [[4,3,2,-1],
+     *              [3,2,1,-1],
+     *              [1,1,-1,-2],
+     *              [-1,-1,-2,-3]]
      * 输出：8
      * 解释：矩阵中共有 8 个负数。
      *
@@ -35,7 +38,7 @@ public class CountNegativeNumbersInASortedMatrix {
      */
 
     /**
-     * 思路：二分法
+     * 思路：二分法；二分法优化，每一次遍历范围缩小；二分法优化，分别查找行和列中第一个小于0的元素，转为一维数组
      * 时间复杂度：O()
      * 空间复杂度：O()
      * 结果:
@@ -45,7 +48,66 @@ public class CountNegativeNumbersInASortedMatrix {
      * @return
      */
     public int countNegatives(int[][] grid) {
+        /**
+         * 遍历二位数组，每一列使用二分法找到第一个小于0的元素
+         * 时间复杂度：O(nlogn)
+         * 空间复杂度：O(1)
+         */
 
-        return -1;
+        /**
+         * 给你一个 m * n 的矩阵 grid，矩阵中的元素无论是按行还是按列，都以<b>非严格递减顺序</b>排列。 请你统计并返回 grid 中 <b>负数</b> 的数目。
+         */
+        int ans = 0;
+        // 行数
+        for (int i = 0; i < grid.length; i++) {
+            // 列数
+            for (int j = 0; j < grid[i].length; j++) {
+                // 对每列使用二分法，查找第一个小于 0 的元素
+                int left = j;
+                int right = grid[i].length - 1;
+                int midel = left + (right - left) / 2;
+                while (left <= right) {
+                    if (midel + 1 < grid[i].length && grid[i][midel] > 0 && grid[i][midel + 1] < 0) {
+                        ans = ans + (grid[i].length - (midel + 1));
+                    } else if (midel - 1 > 0 && grid[i][midel - 1] > 0 && grid[i][midel] < 0) {
+                        ans = ans + (grid[i].length - midel);
+                    } else if (grid[i][midel] < 0) {
+                        left = midel + 1;
+                    } else {
+                        right = midel - 1;
+                    }
+                }
+            }
+        }
+
+        return ans;
     }
+
+    /**
+     * 思路：暴力解法，逐行遍历
+     * 时间复杂度：O(mn)
+     * 空间复杂度：O(1)
+     * 结果:
+     * 优化建议：
+     *
+     * @param grid
+     * @return
+     */
+    public int countNegatives1(int[][] grid) {
+        int ans = 0;
+        // 行数
+        for (int i = 0; i < grid.length; i++) {
+            // 列数
+            for (int j = 0; j < grid[i].length; j++) {
+                // 第 i 行的第 j 列元素
+                if (grid[i][j] < 0) {
+                    ans = ans + (grid[i].length - j);
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+
 }
--- src/test/java/top/cupcupcui/leetcodeproblems/binarysearch/CountNegativeNumbersInASortedMatrixTest.java
+++ src/test/java/top/cupcupcui/leetcodeproblems/binarysearch/CountNegativeNumbersInASortedMatrixTest.java
@@ -34,4 +34,46 @@ public class CountNegativeNumbersInASortedMatrixTest {
 
         Assert.assertEquals(expected, response);
     }
+
+    /**
+     * 输入：grid = {{0}}
+     * 输出：0
+     */
+    @Test
+    public void test_PositiveCase2() {
+        CountNegativeNumbersInASortedMatrix service = new CountNegativeNumbersInASortedMatrix();
+        int[][] arg1 = {{0}};
+        int expected = 0;
+        int response = service.countNegatives(arg1);
+
+        Assert.assertEquals(expected, response);
+    }
+
+    /**
+     * 输入：grid = {{-1}}
+     * 输出：1
+     */
+    @Test
+    public void test_PositiveCase3() {
+        CountNegativeNumbersInASortedMatrix service = new CountNegativeNumbersInASortedMatrix();
+        int[][] arg1 = {{-1}};
+        int expected = 1;
+        int response = service.countNegatives(arg1);
+
+        Assert.assertEquals(expected, response);
+    }
+
+    /**
+     * 输入：grid = {{0,-1},{-1,-1}}
+     * 输出：3
+     */
+    @Test
+    public void test_PositiveCase4() {
+        CountNegativeNumbersInASortedMatrix service = new CountNegativeNumbersInASortedMatrix();
+        int[][] arg1 = {{0, -1}, {-1, -1}};
+        int expected = 3;
+        int response = service.countNegatives(arg1);
+
+        Assert.assertEquals(expected, response);
+    }
 }
