96d4936c26e78d9500c0c9607ae9f3234876fa4b
cuiguanghao <cuiguanghao@nantian.com.cn>
2025-12-26 16:19:19 +0800

    1351. 统计有序矩阵中的负数，二分法，提交未通过

--- src/main/java/top/cupcupcui/leetcodeproblems/binarysearch/CountNegativeNumbersInASortedMatrix.java
+++ src/main/java/top/cupcupcui/leetcodeproblems/binarysearch/CountNegativeNumbersInASortedMatrix.java
@@ -49,7 +49,39 @@ public class CountNegativeNumbersInASortedMatrix {
      */
     public int countNegatives(int[][] grid) {
         /**
-         * 遍历二位数组，每一列使用二分法找到第一个小于0的元素
+         * 遍历二位数组，每一列使用二分法找到<b>第一个小于0</b>的元素
+         * 时间复杂度：O(nlogn)
+         * 空间复杂度：O(1)
+         */
+
+        /**
+         * 给你一个 m * n 的矩阵 grid，矩阵中的元素无论是按行还是按列，都以<b>非严格递减顺序</b>排列。 请你统计并返回 grid 中 <b>负数</b> 的数目。
+         */
+        int ans = 0;
+        // 行数
+        for (int i = 0; i < grid.length; i++) {
+            // 列数
+            for (int j = 0; j < grid[i].length; j++) {
+
+            }
+        }
+
+        return ans;
+    }
+
+    /**
+     * 思路：二分法；二分法优化，每一次遍历范围缩小；
+     * 时间复杂度：O()
+     * 空间复杂度：O()
+     * 结果: 提交未通过
+     * 优化建议：
+     *
+     * @param grid
+     * @return
+     */
+    public int countNegatives2(int[][] grid) {
+        /**
+         * 遍历二位数组，每一列使用二分法找到<b>第一个小于0</b>的元素
          * 时间复杂度：O(nlogn)
          * 空间复杂度：O(1)
          */
@@ -65,12 +97,15 @@ public class CountNegativeNumbersInASortedMatrix {
                 // 对每列使用二分法，查找第一个小于 0 的元素
                 int left = j;
                 int right = grid[i].length - 1;
-                int midel = left + (right - left) / 2;
+                int midel = left + (right - left + 1) / 2;
+
                 while (left <= right) {
                     if (midel + 1 < grid[i].length && grid[i][midel] > 0 && grid[i][midel + 1] < 0) {
                         ans = ans + (grid[i].length - (midel + 1));
+                        break;
                     } else if (midel - 1 > 0 && grid[i][midel - 1] > 0 && grid[i][midel] < 0) {
                         ans = ans + (grid[i].length - midel);
+                        break;
                     } else if (grid[i][midel] < 0) {
                         left = midel + 1;
                     } else {
