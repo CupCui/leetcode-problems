87270256e51eba5e60bf0466cd0c69cd14fddc61
cuiguanghao <cuiguanghao@nantian.com.cn>
2026-01-28 10:28:50 +0800

    15. 三数之和，初步提交

---
+++ src/main/java/top/cupcupcui/leetcodeproblems/sorting/ThreeSum.java
@@ -0,0 +1,112 @@
+package top.cupcupcui.leetcodeproblems.sorting;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author cuiguanghao
+ * @date 2026/1/27 17:15
+ * @description 15. 三数之和
+ */
+public class ThreeSum {
+    /**
+     * https://leetcode.cn/problems/3sum/description/?envType=problem-list-v2&envId=sorting&
+     * 15. 三数之和
+     * 中等
+     * 提示
+     * 给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != k ，
+     * 同时还满足 nums[i] + nums[j] + nums[k] == 0 。请你返回所有和为 0 且不重复的三元组。
+     *
+     * 注意：答案中不可以包含重复的三元组。
+     *
+     * 示例 1：
+     * 输入：nums = [-1,0,1,2,-1,-4]
+     * 输出：[[-1,-1,2],[-1,0,1]]
+     * 解释：
+     * nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0 。
+     * nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0 。
+     * nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0 。
+     * 不同的三元组是 [-1,0,1] 和 [-1,-1,2] 。
+     * 注意，输出的顺序和三元组的顺序并不重要。
+     *
+     * 示例 2：
+     * 输入：nums = [0,1,1]
+     * 输出：[]
+     * 解释：唯一可能的三元组和不为 0 。
+     *
+     * 示例 3：
+     * 输入：nums = [0,0,0]
+     * 输出：[[0,0,0]]
+     * 解释：唯一可能的三元组和为 0 。
+     *
+     * 提示：
+     * 3 <= nums.length <= 3000
+     * -105 <= nums[i] <= 105
+     *
+     * 相关标签
+     * 数组 双指针 排序
+     */
+
+
+    /**
+     * 思路：双指针 排序
+     * 时间复杂度：O()
+     * 空间复杂度：O()
+     * 结果:
+     * 优化建议：
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+        /**
+         * 排序：nums = [-4,-1,-1,0,1,2]
+         * i    l     r    ans
+         * -4   -4    2    l=-1
+         *
+         * i    l     r    ans
+         * -1   -4    2    r=1
+         *
+         * i    l     r    ans
+         * -1   -4    1    r=1
+         *
+         *
+         * 输出：[[-1,-1,2],[-1,0,1]]
+         */
+
+        // 使用快排，对数组进行排序
+        // 基准选择：确定一个 pivot 元素作为比较标准
+        int l = 0;
+        int h = nums.length - 1;
+
+        partition(nums, l, h);
+
+        return Collections.singletonList(Arrays.stream(nums).boxed().collect(Collectors.toList()));
+    }
+
+    private static void partition(int[] nums, int l, int h) {
+        int pivot = h;
+        int i = l;
+        for (int j = l; j <= h; j++) {
+            if (nums[j] < nums[pivot]) {
+                swap(nums, i, j);
+                i++;
+            }
+        }
+        swap(nums, i, pivot);
+        // 退出递归的条件
+        if (i == h) {
+            return;
+        }
+        // partition left
+        partition(nums, l, i - 1);
+        // partition right
+        partition(nums, i, h);
+
+    }
+
+    private static void swap(int[] nums, int source, int target) {
+        int temp = nums[source];
+        nums[source] = nums[target];
+        nums[target] = temp;
+    }
+}
---
+++ src/test/java/top/cupcupcui/leetcodeproblems/sorting/ThreeSumTestV2.java
@@ -0,0 +1,64 @@
+package top.cupcupcui.leetcodeproblems.sorting;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ThreeSumTestV2 {
+
+    @Test
+    public void test_PositiveCase0() {
+        ThreeSum service = new ThreeSum();
+        int[] arg1 = {-1, 0, 1, 2, -1, 1};
+        // int[] arg1 = {-1, 0, -1, 1, 1, 2};
+        List<List<Integer>> expected = Arrays.asList(Arrays.asList(-1, 0, -1, 1, 1, 2));
+        List<List<Integer>> response = service.threeSum(arg1);
+        for (int i = 0; i < expected.size(); i++) {
+            Assert.assertArrayEquals(expected.get(i).toArray(), response.get(i).toArray());
+        }
+    }
+
+    @Test
+    public void test_PositiveCase1() {
+        ThreeSum service = new ThreeSum();
+        int[] arg1 = {-1, 0, 3, 4, -1, -4, 2};
+        // int[] arg1 = {-1, 0, -1, -4, 2, 4, 3};
+        List<List<Integer>> expected = Arrays.asList(Arrays.asList(-1, 0, -1, -4, 2, 4, 3));
+        List<List<Integer>> response = service.threeSum(arg1);
+        for (int i = 0; i < expected.size(); i++) {
+            Assert.assertArrayEquals(expected.get(i).toArray(), response.get(i).toArray());
+        }
+    }
+
+/*     @Test
+    public void test_PositiveCase1() {
+        ThreeSum service = new ThreeSum();
+        int[] arg1 = {-1, 0, 3, 4, -1, -4, 2};
+        // int[] arg1 = {-1, 0, -1, -4, 2, 4, 3};
+        int[] arg1 = {-1, 0, -1, -4};
+        int[] arg1 = {-4, 0, -1, -1};
+
+        int[] arg1 = {-4, 0, -1, -1};
+        int[] arg1 = {-4, -1, 0, -1};
+        int[] arg1 = {-4, -1, -1, 0};
+        int[] arg1 = {-4, -1, -1, 0};
+
+
+
+        // int[] arg1 = {2, 4, 3};
+        // int[] arg1 = {2, 3, 4};
+        int[] arg1 = {3, 4};
+        int[] arg1 = {3, 4};
+
+        List<List<Integer>> expected = Arrays.asList(Arrays.asList(-1, 0, -1, -4, 2, 4, 3));
+        List<List<Integer>> response = service.threeSum(arg1);
+        for (int i = 0; i < expected.size(); i++) {
+            Assert.assertArrayEquals(expected.get(i).toArray(), response.get(i).toArray());
+        }
+    } */
+
+}
