0d6e28af8fc1fc1c92d47ee5c1a9e980e464f521
cuiguanghao <cuiguanghao@nantian.com.cn>
2026-02-05 10:22:45 +0800

    16. 最接近的三数之和，初步提交

--- src/main/java/top/cupcupcui/leetcodeproblems/sorting/ThreeSumClosest.java
+++ src/main/java/top/cupcupcui/leetcodeproblems/sorting/ThreeSumClosest.java
@@ -47,15 +47,73 @@ public class ThreeSumClosest {
     public int threeSumClosest(int[] nums, int target) {
         /**
          * 输入：nums = [-1,2,1,-4], target = 1
+         * 输出：2
+         * 解释：与 target 最接近的和是 2 (-1 + 2 + 1 = 2)。
+         * 排序：nums = [-4,-1,1,2]
+         *
+         *
+         * 遍历 nums 中每个元素，固定当前元素，
+         * 在所有 nums 中找到两个数，使 三个数的和最接近 target
          */
 
-        int l = 0;
-        int h = nums.length - 1;
+        // 排序
+        partition(nums, 0, nums.length - 1);
 
-        partition(nums, l, h);
+        int ans = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            int l = i + 1;
+            int h = nums.length - 1;
+            while (l < h) {
+                /**
+                 * 给你一个长度为 n 的整数数组 nums 和 一个目标值 target。请你从 nums 中选出 **三个** 在 **不同下标位置** 的整数，使它们的和与 target **最接近**。
+                 * 返回这三个数的 **和**。
+                 * 假定每组输入只存在恰好一个解。
+                 * 排序：nums = [-4,-3,-2,-1,0,1,2,3,4]
+                 * nums[i]  l    h   sum  target
+                 * -4       -3   4   -3   1
+                 * nums[i]  l    h   sum  target
+                 * -4       -2   4   -2   1
+                 * nums[i]  l    h   sum  target
+                 * -4       -1   4   -1   1
+                 *
+                 * nums[i]  l    h   sum  target
+                 * -4       0    4   0    1        match
+                 *
+                 * 排序：nums = [0,1,2,3,4,5,6,7]
+                 * nums[i]  l    h   sum  target
+                 * 0        1    7   8    5
+                 * nums[i]  l    h   sum  target
+                 * 0        1    6   7    5
+                 * nums[i]  l    h   sum  target
+                 * 0        1    5   6    5
+                 */
+                // nums 中选出三个数的和，和 target 举例的绝对值，越小越好
+                int sum = nums[l] + nums[h] + nums[i] - target;
+                if (sum == 0) {
+                    return nums[l] + nums[h] + nums[i];
+                } else if (sum < 0) {
+                    if (Math.abs(sum) < ans) {
+                        ans = Math.abs(sum);
+                        l++;
+                    } else if (Math.abs(sum) == ans) {
 
+                    } else if (Math.abs(sum) > ans) {
+                        h--;
+                    }
+                } else {
+                    if (Math.abs(sum) < ans) {
+                        l++;
+                    } else if (Math.abs(sum) == ans) {
 
-        return nums.length;
+                    } else if (Math.abs(sum) > ans) {
+                        ans = Math.abs(sum);
+                        h--;
+                    }
+                }
+            }
+        }
+
+        return ans - target;
     }
 
     private static void partition(int[] nums, int l, int h) {
