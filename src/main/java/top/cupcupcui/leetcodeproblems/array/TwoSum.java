156a1d6eb7cd0814122d2bacc3abb8d397b9a02c
cuiguanghao <cuiguanghao@nantian.com.cn>
2026-02-11 09:36:10 +0800

    1. 两数之和，初始提交

---
+++ src/main/java/top/cupcupcui/leetcodeproblems/array/TwoSum.java
@@ -0,0 +1,67 @@
+package top.cupcupcui.leetcodeproblems.array;
+
+/**
+ * @author cuiguanghao
+ * @date 2026/2/10 17:46
+ * @description 1. 两数之和
+ */
+public class TwoSum {
+    /**
+     * https://leetcode.cn/problems/two-sum/description/?envType=problem-list-v2&envId=array
+     * 1. 两数之和
+     * 简单
+     * 提示
+     * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的**数组下标**。
+     * 你可以假设每种输入**只会对应一个答案**，并且你不能使用两次相同的元素。
+     * 你可以按任意顺序返回答案。
+     *
+     * 示例 1：
+     * 输入：nums = [2,7,11,15], target = 9
+     * 输出：[0,1]
+     * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+     *
+     * 示例 2：
+     * 输入：nums = [3,2,4], target = 6
+     * 输出：[1,2]
+     *
+     * 示例 3：
+     * 输入：nums = [3,3], target = 6
+     * 输出：[0,1]
+     *
+     * 提示：
+     * 2 <= nums.length <= 104
+     * -109 <= nums[i] <= 109
+     * -109 <= target <= 109
+     * 只会存在一个有效答案
+     *
+     * 进阶：你可以想出一个时间复杂度小于 O(n2) 的算法吗？
+     *
+     * 相关标签
+     * 初级工程师 数组 哈希表
+     */
+
+    /**
+     * 思路：
+     * 时间复杂度：O()
+     * 空间复杂度：O()
+     * 结果:
+     * 优化建议：
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum(int[] nums, int target) {
+        int l = 0;
+        int h = nums.length - 1;
+
+        // 快排，分治思维
+        for (int i = l; i < h; i++) {
+
+        }
+
+
+        return null;
+    }
+
+}
