c1a6322e32432539742e86f1bc00a0c2a902983a
cuiguanghao <cuiguanghao@nantian.com.cn>
2026-03-19 17:46:16 +0800

    20. 有效的括号，初始提交

---
+++ src/main/java/top/cupcupcui/leetcodeproblems/geminiinterview20/ValidParentheses.java
@@ -0,0 +1,63 @@
+package top.cupcupcui.leetcodeproblems.geminiinterview20;
+
+/**
+ * 20. 有效的括号
+ *
+ * @author cuiguanghao
+ * @date 2026/3/19 17:43
+ */
+public class ValidParentheses {
+    /**
+     * https://leetcode.cn/problems/valid-parentheses/description/
+     * 20. 有效的括号
+     * 简单
+     *
+     * 提示
+     * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+     *
+     * 有效字符串需满足：
+     * 左括号必须用相同类型的右括号闭合。
+     * 左括号必须以正确的顺序闭合。
+     * 每个右括号都有一个对应的相同类型的左括号。
+     *
+     * 示例 1：
+     * 输入：s = "()"
+     * 输出：true
+     *
+     * 示例 2：
+     * 输入：s = "()[]{}"
+     * 输出：true
+     *
+     * 示例 3：
+     * 输入：s = "(]"
+     * 输出：false
+     *
+     * 示例 4：
+     * 输入：s = "([])"
+     * 输出：true
+     *
+     * 示例 5：
+     * 输入：s = "([)]"
+     * 输出：false
+     *
+     * 提示：
+     * 1 <= s.length <= 104
+     * s 仅由括号 '()[]{}' 组成
+     */
+
+    /**
+     * 思路：
+     * 时间复杂度：O()
+     * 空间复杂度：O()
+     * 结果:
+     * 优化建议：
+     *
+     * @param s
+     * @return
+     */
+    public boolean isValid(String s) {
+
+        return false;
+    }
+
+}
