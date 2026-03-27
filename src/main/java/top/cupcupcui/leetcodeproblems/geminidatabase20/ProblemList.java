decdef22376718faacce6ea3e76b14d7989b589f
cuiguanghao <cuiguanghao@nantian.com.cn>
2026-03-27 18:02:02 +0800

    add database sql problem list

---
+++ src/main/java/top/cupcupcui/leetcodeproblems/geminidatabase20/ProblemList.java
@@ -0,0 +1,16 @@
+package top.cupcupcui.leetcodeproblems.geminidatabase20;
+
+/**
+ * ProblemList
+ *
+ * @author cuiguanghao
+ * @date 2026/3/27 17:41
+ */
+public class ProblemList {
+    /**
+     * 为了短期快速应对面试sql相关问题，最最经典的 leetcode 数据库查询 sql 题目，
+     * 而且是最最基础的 leetcode 数据库 题目都有哪些。
+     * 从简单到难分别列出一个列表。
+     * https://gemini.google.com/share/38c2b25a917a
+     */
+}
